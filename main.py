0ae3d791516d3e907fed77032d6290781f4f517e
Milos <milosprehnil@gmail.com>
2025-02-21 17:40:39 +0100

    nazev ukolu nemuze byt prazdny

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-
 ukoly = ["Úkol 1 - Uklidit", "Úkol 2 - Vytřít"]
 
+
 def pridat_ukol():
-    nazev_ukolu = str(input("Zadejte název úkolu: "))
+    while True:
+        nazev_ukolu = str(input("Zadejte název úkolu: "))
+        if nazev_ukolu.strip() == "":
+            print("Název úkolu nemůže být prázdný.\n")
+        else:
+            break
+
     popis_ukolu = str(input("Zadejte popis úkolu: "))
-    ukol_s_popisem =  f"{nazev_ukolu} - {popis_ukolu}"
+    if popis_ukolu.strip() == "":
+        popis_ukolu = "Bez popisu."
+        print("Bez popisu.\n")
+    ukol_s_popisem = f"{nazev_ukolu} - {popis_ukolu}"
     ukoly.append(ukol_s_popisem)
     print(f"Úkol '{nazev_ukolu}' byl přidán.\n")
 
@@ -14,7 +23,7 @@ def zobrazit_ukoly():
         print("Seznam úkolů je prázdný. Začněte přidáním úkolu volbou č. 1.\n")
     else:
         print(f"\nSeznam úkolů:")
-        for (index, ukol) in enumerate(ukoly, start=1):
+        for index, ukol in enumerate(ukoly, start=1):
             print(f"{index}. {ukol}")
         print("")
 
@@ -24,23 +33,24 @@ def odstranit_ukol():
         print("Seznam úkolů je prázdný. Začněte přidáním úkolu volbou č. 1.\n")
     else:
         print(f"\nSeznam úkolů:")
-        for (index, ukol) in enumerate(ukoly, start=1):
+        for index, ukol in enumerate(ukoly, start=1):
             print(f"{index}. {ukol}")
         print("")
 
     user_choice = input("Zadejte číslo úkolu, který chcete odstranit: ")
     try:
         user_choice_int = int(user_choice)
-        if user_choice_int in range(len(ukoly)+1):
+        if user_choice_int in range(len(ukoly) + 1):
             # user_choice_int -= 1
-            print(f"Odstranil jsem položku '{ukoly[user_choice_int-1]}' ze seznamu úkolů.\n")
-            ukoly.pop(user_choice_int-1)
+            print(
+                f"Odstranil jsem položku '{ukoly[user_choice_int-1]}' ze seznamu úkolů.\n"
+            )
+            ukoly.pop(user_choice_int - 1)
 
         else:
             print("Zadal jsi číslo mimo rozsah seznamu úkolů.\n")
     except ValueError:
         print("Nezadal jsi číselnou hodnotu.\n")
-
 
 
 def hlavni_menu():
@@ -68,8 +78,7 @@ def hlavni_menu():
     except ValueError:
         print("Nezadal si platné číslo.\n")
 
+
 task_run = True
 while task_run:
     hlavni_menu()
-
-@{}
