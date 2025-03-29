8b8c0028149d60c20b6cd29190761d4599d147b9
Milos <milosprehnil@gmail.com>
2025-03-29 22:36:57 +0100

    vytvoreni nove db plus tri testy

---
+++ test_db.py
@@ -0,0 +1,86 @@
+import pytest
+import mysql.connector
+from main import (
+    pridat_ukol,
+    aktualizovat_ukol,
+    odstranit_ukol,
+    zobrazit_vsechny_ukoly,
+)
+
+
+@pytest.fixture(scope="function")
+def db_setup():
+    """
+    Fixture pro připojení k databázi a nastavení testovacího prostředí.
+    """
+    # Připojení k databázi
+    conn = mysql.connector.connect(
+        host="localhost", user="root", password="engeto", database="engeto_test"
+    )
+    cursor = conn.cursor()
+
+    # Vytvoreni testovaci tabulky
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS ukoly (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nazev VARCHAR(50),
+            popis VARCHAR(500),
+            stav VARCHAR(100) DEFAULT 'Nezahájeno',
+            datum_vytvoreni TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """
+    )
+    conn.commit()
+
+    # Předání připojení a kurzoru testům
+    yield conn, cursor
+
+    # Úklid po testech: Smazání tabulky
+    cursor.execute("DROP TABLE IF EXISTS ukoly")
+    conn.commit()
+
+    # Uzavření připojení
+    cursor.close()
+    conn.close()
+
+
+def test_pridat_ukol(db_setup):
+    conn, cursor = db_setup
+    pridat_ukol(conn, nazev_ukolu="Ukol1", popis_ukolu="Popis1")
+    # Ověření vložení
+    cursor.execute("SELECT * FROM ukoly WHERE nazev = 'Ukol1'")
+    result = cursor.fetchone()
+
+    assert result is not None, "Záznam nebyl vložen do tabulky."
+    assert result[1] == "Ukol1", "Název úkolu není správný."
+    assert result[2] == "Popis1", "Popis úkolu není správný."
+
+
+def test_pridat_prazdny_nazev_ukol(db_setup):
+    conn, cursor = db_setup
+    pridat_ukol(conn, nazev_ukolu="", popis_ukolu="Popis1")
+    # Ověření vložení
+    cursor.execute("SELECT * FROM ukoly WHERE nazev = 'Ukol1'")
+    result = cursor.fetchone()
+
+    # assert result is None, "Prázdný záznam byl vložen do tabulky."
+    # Ověření, že tabulka je prázdná
+    cursor.execute("SELECT COUNT(*) FROM ukoly")
+    count = cursor.fetchone()[0]
+    assert count == 0, "Tabulka by měla být prázdná."
+
+
+def test_aktualizovat_ukol(db_setup):
+    conn, cursor = db_setup
+    # Přidání úkolu pro aktualizaci
+    pridat_ukol(conn, nazev_ukolu="Ukol1", popis_ukolu="Popis1")
+    # Aktualizace úkolu
+    aktualizovat_ukol(conn, id_ukolu=1, novy_stav="Probíhá")
+    # Ověření aktualizace
+    cursor.execute("SELECT * FROM ukoly WHERE id = 1")
+    result = cursor.fetchone()
+    assert result[3] == "Probíhá", "Stav úkolu nebyl aktualizován správně."
+    assert result[1] == "Ukol1", "Název úkolu nebyl správně zachován."
+    assert result[2] == "Popis1", "Popis úkolu nebyl správně zachován."
+    assert result[4] is not None, "Datum vytvoření úkolu by mělo být nastaveno."
