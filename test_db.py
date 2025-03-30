4d6b8587673f93442c313b7ecffaf9e727396e22
Milos <milosprehnil@gmail.com>
2025-03-30 13:14:52 +0200

    drobne upravy

--- test_db.py
+++ test_db.py
@@ -20,7 +20,7 @@ def db_setup():
     )
     cursor = conn.cursor()
 
-    # Vytvoreni testovaci tabulky
+    # Vytvořeni testovaci tabulky
     cursor.execute(
         """
         CREATE TABLE IF NOT EXISTS ukoly (
@@ -47,9 +47,9 @@ def db_setup():
 
 
 def test_pridat_ukol(db_setup):
+    # Přidání úkolu do databáze
     conn, cursor = db_setup
     pridat_ukol(conn, nazev_ukolu="Ukol1", popis_ukolu="Popis1")
-    # Ověření vložení
     cursor.execute("SELECT * FROM ukoly WHERE nazev = 'Ukol1'")
     result = cursor.fetchone()
     assert result is not None, "Záznam nebyl vložen do tabulky."
@@ -58,37 +58,37 @@ def test_pridat_ukol(db_setup):
 
 
 def test_pridat_prazdny_nazev_ukol(db_setup):
+    # Přidání úkolu s prázdným názvem
     conn, cursor = db_setup
     pridat_ukol(conn, nazev_ukolu="", popis_ukolu="Popis1")
-    # Ověření vložení
     cursor.execute("SELECT COUNT(*) FROM ukoly")
     count = cursor.fetchone()[0]
     assert count == 0, "Tabulka by měla být prázdná."
 
 
 def test_pridat_ukol_s_dlouhym_nazvem(db_setup):
+    # Přidání úkolu s dlouhým názvem
     conn, cursor = db_setup
     with pytest.raises(ValueError, match="Název úkolu je příliš dlouhý"):
         pridat_ukol(conn, nazev_ukolu="x" * 51, popis_ukolu="Popis1")
 
 
-# @pytest.mark.testuji
 def test_zobrazit_probihajici_ukoly(db_setup):
     conn, cursor = db_setup
-    # Přidání několika úkolů
+    # Přidání úkolů
     for i in range(0, 5):
         pridat_ukol(conn, nazev_ukolu=f"Ukol{i}", popis_ukolu=f"Popis{i}")
 
     aktualizovat_ukol(conn, id_ukolu=1, novy_stav="Probíhá")
-    # Zobrazení všech úkolů
+    # Zobrazení úkolů se stavem "Probíhá"
     cursor.execute("SELECT * FROM ukoly where stav = 'Probíhá'")
     ukoly = cursor.fetchall()
     assert len(ukoly) == 1, "Nesprávný počet úkolů."
-
+    # Zobrazení všech úkolů
     cursor.execute("SELECT * FROM ukoly")
     vsechny_ukoly = cursor.fetchall()
     assert len(vsechny_ukoly) == 5, "Nesprávný počet úkolů."
-
+    # Zobrazení úkolů, které nejsou "Probíhá"
     cursor.execute("SELECT * FROM ukoly WHERE stav != 'Probíhá'")
     ukoly_neprobihaji = cursor.fetchall()
     assert len(ukoly_neprobihaji) == 4, "Nesprávný počet úkolů."
